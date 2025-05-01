0e0e0b9f9b8840e336b0ecfdf9ab0b005fc3d975
Mert Arcan <62708030+Arjein@users.noreply.github.com>
2025-05-01 17:08:18 +0300

    bug fix

--- user.py
+++ user.py
@@ -107,7 +107,7 @@ class AppUser:
             from cosmos_db import CosmosDB
             cosmos_db = CosmosDB.get_instance()
             # TODO EDIT THIS
-            return  #cosmos_db.get_user_last_email_time(cls.email)
+            return  cosmos_db.get_user_last_email_time(cls.email)
         except Exception as e:
             logger = logging.getLogger("ClaraSecretary")
             logger.error(f"Error getting last email update time: {e}")
